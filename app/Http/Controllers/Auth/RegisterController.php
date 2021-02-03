5fe158111ff6c31ed5fbd7da57a0a1def46eff75
Trauvin <mateusgalote@gmail.com>
2021-02-03 16:03:12 -0300

    authentication implementation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -10,5 +12,29 @@ class RegisterController extends Controller
     public function index()
     {
         return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed',
+
+
+        ]);
+        
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('dashboard');
+        // store user
+        // sign in the user in
+        // redirect
     }
 }
